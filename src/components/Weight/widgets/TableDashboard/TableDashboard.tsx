faad5208fb6679ac7aa84c7efea183831a955180
mollyytran <mtrann91@gmail.com>
2026-08-18 18:34:11 -0700

    Fix weight unit to consider user selection on metric of imperial system

--- src/components/Weight/widgets/TableDashboard/TableDashboard.tsx
+++ src/components/Weight/widgets/TableDashboard/TableDashboard.tsx
@@ -4,10 +4,10 @@ import { WeightEntry } from "@/components/Weight/models/WeightEntry";
 import React from 'react';
 import { useTranslation } from "react-i18next";
 import { dateTimeToLocale } from "@/core/lib/date";
+import { useProfileQuery } from "@/components/User";
 
 
 const PREFIX = 'WeightTableDashboard';
-
 const classes = {
     table: `${PREFIX}-table`
 };
@@ -31,9 +31,14 @@ export interface WeightTableProps {
 export const WeightTableDashboard = ({ weights }: WeightTableProps) => {
     const [t] = useTranslation();
 
+    const profileQuery = useProfileQuery();
+    const isMetric = profileQuery.data?.useMetric ?? true;
+
     const WEIGHT_ENTRIES_TO_SHOW = 5;
 
     const filteredWeight = weights.slice(0, WEIGHT_ENTRIES_TO_SHOW);
+
+    const KG_TO_LB = 2.20462;
 
     return (
         <Root className={classes.table}>
@@ -42,14 +47,14 @@ export const WeightTableDashboard = ({ weights }: WeightTableProps) => {
                     <TableHead>
                         <TableRow>
                             <TableCell>{t('date')}</TableCell>
-                            <TableCell>{t('weight')}</TableCell>
+                            <TableCell>{t('weight')} ({isMetric ? 'kg' : 'lb'})</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {filteredWeight.map((row) => (
                             <TableRow key={row.date.toISOString()}>
                                 <TableCell>{dateTimeToLocale(row.date)}</TableCell>
-                                <TableCell>{row.weight}</TableCell>
+                                <TableCell>{isMetric ? row.weight : Math.round(row.weight * KG_TO_LB)}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
